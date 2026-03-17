c490cedee57050debd68bd93a0db42b3f8d9e664
KRITIKA BENJWAL <89837049+Kritika11052005@users.noreply.github.com>
2026-03-17 12:27:46 +0530

    Update db_mysql.php

--- php/config/db_mysql.php
+++ php/config/db_mysql.php
@@ -1,21 +1,18 @@
 <?php
 require_once __DIR__ . '/env_loader.php';
-
 // php/config/db_mysql.php
 define('MYSQL_HOST', $_ENV['MYSQL_HOST'] ?? getenv('MYSQL_HOST') ?: 'sql12.freesqldatabase.com');
 define('MYSQL_PORT', (int)($_ENV['MYSQL_PORT'] ?? getenv('MYSQL_PORT') ?: 3306));
-define('MYSQL_DB',   $_ENV['MYSQL_DB']   ?? getenv('MYSQL_DB')   ?: 'sql12819356');
-define('MYSQL_USER', $_ENV['MYSQL_USER'] ?? getenv('MYSQL_USER') ?: 'sql12819356');
-define('MYSQL_PASS', $_ENV['MYSQL_PASS'] ?? getenv('MYSQL_PASS') ?: 'm9RFHXdpaF');
+define('MYSQL_DB',   $_ENV['MYSQL_DB']   ?? getenv('MYSQL_DB')   ?: 'sql12820348');
+define('MYSQL_USER', $_ENV['MYSQL_USER'] ?? getenv('MYSQL_USER') ?: 'sql12820348');
+define('MYSQL_PASS', $_ENV['MYSQL_PASS'] ?? getenv('MYSQL_PASS') ?: 'HD8tSTJnjk');
 
 $mysqli = new mysqli(MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB, MYSQL_PORT);
-
 if ($mysqli->connect_error) {
     die(json_encode([
         "success" => false, 
         "message" => "Database connection failed: " . $mysqli->connect_error
     ]));
 }
-
 // Return the connection
 return $mysqli;
